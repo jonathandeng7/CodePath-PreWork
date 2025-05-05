445e3c12970fae42a0e367fbe7367bb78c79b175
jonathandeng7 <jmdeng05@gmail.com>
2025-05-04 21:52:13 -0400

    adding prework files to repo

---
+++ Jonathan Deng PreWorkTests/Jonathan_Deng_PreWorkTests.swift
@@ -0,0 +1,17 @@
+//
+//  Jonathan_Deng_PreWorkTests.swift
+//  Jonathan Deng PreWorkTests
+//
+//  Created by Jonathan on 5/4/25.
+//
+
+import Testing
+@testable import Jonathan_Deng_PreWork
+
+struct Jonathan_Deng_PreWorkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
